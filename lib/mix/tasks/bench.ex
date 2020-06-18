e4c4f1521c24685101bb2673f941520e20285178
Pablo Gomez <pablonahuelgomez@gmail.com>
2020-06-18 19:10:08 -0300

    Add benchmark task

---
+++ lib/mix/tasks/bench.ex
@@ -0,0 +1,23 @@
+defmodule Mix.Tasks.Bench do
+  use Mix.Task
+
+  alias Fogwill.Algorithms.Concurrent
+  alias Fogwill.Algorithms.Recursive
+
+  @shortdoc "Benchmarks algorithms"
+  def run(_) do
+    Benchee.run(
+      %{
+        "Concurrent" => &Concurrent.mind(&1),
+        "Recursive" => &Recursive.mind(&1)
+      },
+      inputs: %{
+        "Aira" => 'Aira',
+        "Borges" => 'Borges',
+        "Levrero" => 'Levrero',
+        "Benesdra" => 'Benesdra'
+      },
+      time: 3
+    )
+  end
+end
